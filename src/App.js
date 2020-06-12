934dfcb686160a9e640f4157785c3b36f56c2410
Kyawkhaing444 <kyawkyawkhaing@ucsm.edu.mm>
2020-06-12 14:06:03 +0630

    day3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import './App.css'
-import Header from './components/Header/Header'
-import TeslaBatteryContainer from './containers/TeslaBattery'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import TeslaBatteryContainer from "./containers/TeslaBattery";
 
 function App() {
-    return (
-      <div>
-        <Header/>
-        <TeslaBatteryContainer/>
-      </div>
-    )
+  return (
+    <div>
+      <Header />
+      <TeslaBatteryContainer />
+    </div>
+  );
 }
 
 export default App;
--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,13 +1,30 @@
 import React from "react";
-import './TeslaBattery.css';
-class TeslaBattery extends React.Component{
-    render(){
-        return (
-            <form className="tesla-battery">
-                <h1> Range Per Charge</h1>
-            </form>
-        )
-    }
+import "./TeslaBattery.css";
+import TeslaNotice from "../components/TeslaNotice/TeslaNotice";
+import TeslaCar from "../components/TeslaCar/TeslaCar";
+class TeslaBattery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      carstats: [],
+      config: {
+        speed: 55,
+        temperature: 20,
+        climate: true,
+        wheels: 19,
+      },
+    };
+  }
+  render() {
+    const { config } = this.state;
+    return (
+      <form className="tesla-battery">
+        <h1> Range Per Charge </h1>
+        <TeslaCar wheelsize={config.wheels} />
+        <TeslaNotice />
+      </form>
+    );
+  }
 }
 
 export default TeslaBattery;
